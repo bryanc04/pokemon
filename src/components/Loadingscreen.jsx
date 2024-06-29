145018ec39d2694546cd05aa6ec70fdf224000b0
bryanc04 <87562897+bryanc04@users.noreply.github.com>
2024-06-30 08:55:24 +0900

    f

--- src/components/Loadingscreen.jsx
+++ src/components/Loadingscreen.jsx
@@ -5,7 +5,7 @@ const Loadingscreen = ({percent}) => {
   return (
 <div style={{width: "100vw", height: "100vh"}}>
 <div style={{marginLeft: "auto", marginRight: "auto", fontSize: "100px", display: "flex", alignItems: "center", justifyContent: "center", height: "inherit", width: "fit-content"}}>
-    {percent.toString().substring(0,4)}%(</div>
+    {Math.trunc(percent)}%</div>
           
 </div>
   );
