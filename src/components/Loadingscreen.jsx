48efc54c374bbddf08776e4f541d66c52f79984c
bryanc04 <87562897+bryanc04@users.noreply.github.com>
2024-06-30 08:52:48 +0900

    f

--- src/components/Loadingscreen.jsx
+++ src/components/Loadingscreen.jsx
@@ -5,7 +5,7 @@ const Loadingscreen = ({percent}) => {
   return (
 <div style={{width: "100vw", height: "100vh"}}>
 <div style={{marginLeft: "auto", marginRight: "auto", fontSize: "100px", display: "flex", alignItems: "center", justifyContent: "center", height: "inherit", width: "fit-content"}}>
-    {percent.substring(0,4)}%(</div>
+    {percent.toString().substring(0,4)}%(</div>
           
 </div>
   );
